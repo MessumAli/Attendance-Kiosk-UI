0f3d71e72c99294c926edaacde992db60111ff8b
unknown <messum@ST-SYS-20.symtera.net>
2024-03-15 13:51:25 +0500

    updated dist

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,3 +1,4 @@
+import "../index.css";
 import React, { useState, useEffect, useRef } from "react";
 import { debounce } from "lodash";
 import axios from "axios";
@@ -5,6 +6,8 @@ import Webcam from "react-webcam";
 import Logo from "../assets/logo.png";
 import Green from "../assets/green.png";
 import Blue from "../assets/blue.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 const MainPage = () => {
   const [rfId, setRFID] = useState("Log-In");
@@ -120,25 +123,33 @@ const MainPage = () => {
 
       <div className="flex items-center justify-between sm:flex-col lg:flex-row sm:mt-28 lg:mt-20">
         <div className="flex flex-col md:mr-44 lg:ml-36 ">
-          <p className="text-white text-7xl">Welcome to </p>
-          <p className="pb-2 mt-5 overflow-hidden text-6xl font-bold animate-typing text-lime-500">
+          <p className="text-white text-7xl">Welcome to</p>
+          <p className="pb-2 mt-5 overflow-hidden text-5xl font-bold animate-typing text-lime-500">
             Symtera&nbsp;Technologies
           </p>
 
-          {/* <div className="w-auto"> */}
-          <select
-            className="p-2 mx-4 mt-10 border text-3xl font-bold rounded-md border-gray-300 sm:w-[650px] md:w-80 lg:w-72"
-            value={attendanceLog}
-            onChange={(e) => setAttendanceLog(e.target.value)}
-          >
-            <option value="Log-In">Log-In</option>
-            <option value="Log-Out">Log-Out</option>
-            <option value="On-Break">On-Break</option>
-            <option value="Off-Break">Off-Break</option>
-            <option value="Officially-Log-Out">Officially-Log-Out</option>
-            <option value="Officially-Log-In">Officially-Log-In</option>
-          </select>
-          {/* </div> */}
+          <div className="relative">
+            <span className=" absolute top-[52px] left-[600px]">
+              <FontAwesomeIcon icon={faArrowDown} size="2x" />
+            </span>
+            <select
+              className="select p-2 mx-4 mt-10 border text-3xl font-bold rounded-md border-gray-300 sm:w-[650px] md:w-80 lg:w-72"
+              value={attendanceLog}
+              onChange={(e) => setAttendanceLog(e.target.value)}
+              style={{
+                WebkitAppearance: "none",
+                MozAppearance: "none",
+                appearance: "none",
+              }}
+            >
+              <option value="Log-In">Log-In</option>
+              <option value="Log-Out">Log-Out</option>
+              <option value="On-Break">On-Break</option>
+              <option value="Off-Break">Off-Break</option>
+              <option value="Officially-Log-Out">Officially-Log-Out</option>
+              <option value="Officially-Log-In">Officially-Log-In</option>
+            </select>
+          </div>
         </div>
         <div
           className="z-10 mt-[-80px]"
